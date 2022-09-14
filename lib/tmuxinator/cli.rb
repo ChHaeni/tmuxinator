9efb2cf38b88fcd90438f1a11caaba1d946febdd
Christoph Häni <christoph.haeni@bfh.ch>
2022-09-14 23:01:54 +0200

    Improve if stop is called w/o arg outside mux project

--- lib/tmuxinator/cli.rb
+++ lib/tmuxinator/cli.rb
@@ -254,6 +254,9 @@ module Tmuxinator
           rescue SyntaxError, StandardError => error
             raise "Failed to parse .sessions file: #{error.message}"
           end
+          if yaml.nil?
+            exit!('No tmuxinator session running')
+          end
           return yaml[session_name]
         end
         # FIXME: what if no session file?
@@ -371,7 +374,7 @@ module Tmuxinator
       if name.nil?
         # get calling tmux session name (remove newline at end)
         session_name = `tmux display-message -p '\#{session_name}' | sed 's/^\\(.*\\)$/\\1/'`.chomp()
-        name = get_parent(session_name)
+        name = get_parent(session_name) || exit!('Not inside a tmuxinator project!')
       else
         # project-config takes precedence over a named project in the case that
         # both are provided.
